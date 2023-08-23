7a4e134472703c2659f256f1fc24bd6d20f40968
roberto <robpalacios1@hotmail.com>
2023-08-23 16:42:39 -0500

    add component for CallToAction

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CallToAction = () => {
+  return (
+    <section className="py-5 bg-primary text-white text-center">
+      <div className="container">
+        <h2>¿Listo para comenzar?</h2>
+        <p className="lead">¡Únete a nosotros y experimenta lo increíble!</p>
+        <a href="#" className="btn btn-light btn-lg">Regístrate Ahora</a>
+      </div>
+    </section>
+  );
+}
+
+export default CallToAction;
